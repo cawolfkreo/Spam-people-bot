49d3d99844e7da5bc573213f23cf41b9dc6752e3
Cawolf <camilozambranovotto@hotmail.com>
2023-02-28 01:10:40 -0500

    Re-introducing express :godmode:
    I hope adding I can add express one more time. I definitely wanted a
    webserver I can hook later to some static files + api and control the
    bot from a browser and not just from telegram (plus I might be able to
    have finer control over victims and their messages).
    
    Re-introducing express :godmode:
    
    I hope adding I can add express one more time. I definitely wanted a
    webserver I can hook later to some static files + api and control the
    bot from a browser and not just from telegram (plus I might be able to
    have finer control over victims and their messages).

--- imports/server.js
+++ imports/server.js
@@ -10,25 +10,20 @@ if (!PORT) {
 }
 
 const express = require("express");
-// const fastify = require("fastify");
 const packageInfo = require("../package.json");
 const { printLog, printErrorMsg } = require("./utilities");
 
 /* ======================================
 *			Server config
  ====================================== */
-/*const app = fastify({ logger: true });
-app.register(require("@fastify/formbody"));*/
 
 const app = express();
 
 app.use((req, res, next) => {
 	if (req.path !== "/") {
-		printLog("Called some other path.");
 		next();
 		return;
 	}
-	printLog("Called the main path!");
 	res.append("Access-Control-Allow-Origin", ["https://keepnavion.cawolf.repl.co"]);
 	res.append("Access-Control-Allow-Methods", "GET");
 	res.append("Access-Control-Allow-Headers", "Content-Type");
@@ -40,13 +35,7 @@ app.get("/", (req, res) => {
 	res.send({ version: packageInfo.version });
 });
 
-/* function startServerWithHooks(secretPath, botMiddleware) {
-	printLog(`The secret path is ${secretPath}`);
-	app.post(`/telegraf/${secretPath}`, (req, res) => botMiddleware(req.raw, res.raw));
-	startServer();
-} */
-
-function startServerWithHooks(secretPath, botMiddleware) {
+function startServerWithHooks(botMiddleware) {
 	app.use(botMiddleware);
 	startServer();
 }
@@ -62,23 +51,6 @@ async function startServer() {
 		console.error(err);
 		next();
 	});
-
-	/*app.setErrorHandler((error, req, res)=>{
-		const mainMessage = "Found an error on the fastify level!!";
-		const request = JSON.stringify(req, null, 2);
-		printErrorMsg(mainMessage);
-		printErrorMsg(request);
-		printErrorMsg("================Full error:===================");
-		console.error(error);
-		res.send(error);
-	});*/
-
-	/* try {
-		const host = await app.listen({port: PORT, host: "::"});
-		printLog(`Express web server ready! :D - on host: ${host}`);
-	} catch (err) {
-		app.log.error(err);
-	} */
 
 	const server = app.listen(PORT ,async ()=>{
 		const host = server.address().address;
